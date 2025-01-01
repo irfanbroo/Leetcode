76a764d5195648356ab9412705283ae86cc68ac0
IRFAN S <123147439+irfanbroo@users.noreply.github.com>
2025-01-01 20:36:43 +0530

    Update Missing_Number.py

--- Missing_Number.py
+++ Missing_Number.py
@@ -1,11 +1,9 @@
-##Here, we are using the xor operator 
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
-        xor1 = nums[0]
-        
-        for i in range(1,n):
-            xor1 = xor1 ^ nums[i]
-        for i in range(n+1):
-            xor1 = xor1 ^ i
+        xor1 = 0
+        for i in range(1, n+1):
+            xor1 ^=i
+        for i in range(n):
+            xor1 ^=nums[i]
         return xor1
