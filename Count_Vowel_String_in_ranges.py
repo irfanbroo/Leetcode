27cd41eaf5fbf4dead2e1ecc71fc6247256964b9
IRFAN S <123147439+irfanbroo@users.noreply.github.com>
2025-03-04 18:07:22 +0530

    Update Count_Vowel_String_in_ranges.py

--- Count_Vowel_String_in_ranges.py
+++ Count_Vowel_String_in_ranges.py
@@ -16,3 +16,32 @@ class Solution:
         return arr
         
         
+# Another more simpler version
+
+class Solution:
+    def check_l(self, c):
+        if c in "a,e,i,o,u":
+            return True
+
+    def check(self, word):
+        n = len(word)
+        if self.check_l(word[0]) and self.check_l(word[-1]):
+            return True
+        
+
+    def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
+        ## im thinking about creating a prefix array ;)
+        s = 0
+        arr = []
+        n = len(words)
+        pr = [0] * (n+1)
+        pr[0] = 0
+        for i in range(1,n+1):
+            pr[i] = pr[i-1] + (1 if self.check(words[i-1]) else 0)
+
+        for i, j in queries:
+            s = pr[j+1] - pr[i]
+            arr.append(s)
+
+
+        return arr
