25109562b3e8183d9ad1899a9af748ad43bc5065
IRFAN S <123147439+irfanbroo@users.noreply.github.com>
2025-03-17 21:35:24 +0530

    Update Peak_Index_in_a_moutain_array.py

--- Peak_Index_in_a_moutain_array.py
+++ Peak_Index_in_a_moutain_array.py
@@ -1,5 +1,27 @@
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        n = len(arr)
+        low = 1
+        high = n - 2
+        while low <=high:
+            mid = low + (high-low)//2
+            
+            
+            
+            if arr[mid] > arr[mid+1] and arr[mid] > arr[mid-1]:
+                return mid
+            elif arr[mid] < arr[mid+1]:
+                low = mid+1
+            else:
+                high = mid-1
+
+
+or------------------------------------------------------------------
+
+
+
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
         n = len(arr) - 2
         i = 1
         while i <= n:
@@ -11,3 +33,7 @@ class Solution:
             else:
                 n = mid-1
         return 0 
+
+
+
+        
